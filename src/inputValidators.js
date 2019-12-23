e49ee2e90fc4d2caff489d41afe71e1537a6b273
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2019-12-23 12:53:10 +0530

    added validateN and test for it

--- src/inputValidators.js
+++ src/inputValidators.js
@@ -2,4 +2,23 @@ const getN = function(usrArgs) {
   return usrArgs[usrArgs.indexOf('-n') + 1];
 };
 
-module.exports = { getN };
+const validateN = function(usrArgs) {
+  const index = usrArgs.indexOf('-n');
+  return (
+    index == usrArgs.lastIndexOf('-n') && Number.isInteger(usrArgs[index + 1])
+  );
+};
+
+class errorMsg {
+  constructor() {
+    const errors = {
+      usage: `usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]`,
+      tail: [
+        `tail: option requires an argument -- n`,
+        `tail: illegal offset -- -n`
+      ]
+    };
+    this.error;
+  }
+}
+module.exports = { getN, validateN };
--- test/testInputValidators.js
+++ test/testInputValidators.js
@@ -1,8 +1,17 @@
 const assert = require('chai').assert;
-const { getN } = require('../src/inputValidators.js');
+const { getN, validateN } = require('../src/inputValidators.js');
 
 describe('getN', function() {
   it('should give n value from user args', function() {
     assert.deepStrictEqual(getN(['-n', 5]), 5);
+  });
+  it('should give undefined when there is no -n option present', function() {
+    assert.strictEqual(getN([]), undefined);
+  });
+});
+
+describe('validateN', function() {
+  it('should validateN when it has only one -n and has a integer value after that', function() {
+    assert.isTrue(validateN(['-n', 5]));
   });
 });
