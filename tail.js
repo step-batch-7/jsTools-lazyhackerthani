d71b58bece92a7fa6903426bc8789107260f7cac
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2019-12-23 10:29:09 +0530

    v0.2.0 node tail.js -n 4

--- tail.js
+++ tail.js
@@ -1,7 +1,8 @@
 const Tail = require('./src/tailLib.js');
+const { getN } = require('./src/inputValidators.js');
 
 const main = function(options) {
-  const tail = new Tail();
+  const tail = new Tail(getN(options));
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', line => {
     tail.addLine(line);
